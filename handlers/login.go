8a62a6ae2d6631ed30a608cd1d50679a76cfac25
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-10-17 08:35:49 +0100

    Add login POST

--- handlers/login.go
+++ handlers/login.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 )
 
@@ -8,6 +9,15 @@ func login(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
 		templates["login"].ExecuteTemplate(w, "layout", reCaptchaSiteKey)
-	}
 
+	case "POST":
+		email := r.PostFormValue("email")
+		// password := r.PostFormValue("password")
+
+		var response string
+
+		response = fmt.Sprintf("Welcome %s", email)
+		templates["message"].ExecuteTemplate(w, "layout", messageModel{"Login", response})
+
+	}
 }
